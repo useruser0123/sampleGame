e1a9acaef28dbf8b54d32bfe36f7495cd505a366
Fujiwara <113399284+useruser0123@users.noreply.github.com>
2024-01-16 13:43:35 +0900

    修正

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -1,58 +1,111 @@
-class MyScene extends Phaser.Scene {
+// 他のJSファイルから呼び出された場合はシーンを返す
+class MainScene extends Phaser.Scene {
+    
+    // コンストラクタ
     constructor() {
-        super({ key: 'MyScene1', active: true });
+        // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
+        super('MainScene');
     }
 
+    // シーンの事前読み込み処理
     preload() {
-        this.load.image('background', 'assets/background.png');
-        this.load.image('taro', 'assets/taro.png');
-        this.load.image('hanako', 'assets/hanako.png');
-        this.load.image('apple', 'assets/apple.png');
+         // 画像の読み込み(使用する時の名前, パス)
+        this.load.image('back', 'assets/background.png');
+
+        this.load.image('TARO', 'assets/taro.png');
+        this.load.image('HANAKO', 'assets/hanako.png');
+        this.load.image('APPLE', 'assets/apple.png');
         this.load.image('orange', 'assets/orange.png');
+
+
     }
 
+    // シーン初期化処理
     create() {
-        this.add.image(400, 300, 'background');
-        const taro = this.physics.add.sprite(500, 350, 'taro');
-        const hanako = this.physics.add.sprite(500, 350, 'hanako');
-        this.taro = taro;
-        this.hanako = hanako;
+         // 単体画像をシーンに追加(X座標,Y座標,画像名)
+        this.add.image(400, 300, 'back');
 
-        // ランダムに配置する関数を呼び出し
-        this.placeRandomFruits('apple', 5); // 'apple'を5つ配置
-        this.placeRandomFruits('orange', 5); // 'orange'を5つ配置
-    }
+        const TARO = this.physics.add.sprite(50, 50, 'TARO');
+        const HANAKO = this.physics.add.sprite(750, 400, 'HANAKO');
 
+        this.TARO = TARO
+
+        this.HANAKO = HANAKO
+        let fulte = this.physics.add.staticGroup();
+        for (let index = 0; index < 5; index++) {
+            let  randx1 = Phaser.Math.Between(25, 775) ; 
+            let randy1 =  Phaser.Math.Between(25, 425) ; 
+            fulte.create(randx1, randy1 , 'APPLE'); 
+            let  randx2 = Phaser.Math.Between(25, 775) ; 
+            let randy2 =  Phaser.Math.Between(25, 425) ; 
+            fulte.create(randx2, randy2 , 'orange'); 
+        }
+        this.physics.add.overlap(TARO, fulte, collectCoin, null, this);
+        function collectCoin(p,coin){
+            this.physics.pause();
+            
+        }
+        this.physics.add.overlap(HANAKO, fulte, collectCoin, null, this);
+        function collectCoin(p,coin){
+            this.physics.pause();
+           
+            
+        }
+
+            
+
+        }
+
+     // 毎フレーム実行される繰り返し処理
     update(time, delta) {
-        // キーボードの情報を取得
+
         let cursors = this.input.keyboard.createCursorKeys();
 
-        if (cursors.up.isDown) {
-            this.taro.setVelocityY(-40);
-            this.hanako.setVelocityY(40);
-        } else if (cursors.down.isDown) {
-            this.taro.setVelocityY(40);
-            this.hanako.setVelocityY(-40);
-        } else if (cursors.left.isDown) {
-            this.taro.setVelocityX(-40);
-            this.hanako.setVelocityX(40);
-        } else if (cursors.right.isDown) {
-            this.taro.setVelocityX(40);
-            this.hanako.setVelocityX(-40);
-        } else {
-            this.taro.setVelocityX(0);
-            this.taro.setVelocityY(0);
-            this.hanako.setVelocityX(0);
-            this.hanako.setVelocityY(0);
-        }
-    }
 
-    // ランダムにフルーツを配置する関数
-    placeRandomFruits(fruitKey, count) {
-        for (let i = 0; i < count; i++) {
-            const x = Phaser.Math.Between(25, 775); // 50から750の範囲でランダムなX座標
-            const y = Phaser.Math.Between(25, 425); // 50から550の範囲でランダムなY座標
-            this.add.image(x, y, fruitKey);
+        if(cursors.up.isDown){
+
+            console.log("Up!!");
+
+            this.TARO.setVelocityY(-40);// 上方向の速度を設定
+            this.HANAKO.setVelocityY(40);// 上方向の速度を設定
+
+
+        } else if(cursors.down.isDown){
+
+            console.log("down!!");
+
+            this.TARO.setVelocityY(40);// 下方向の速度を設定
+            this.HANAKO.setVelocityY(-40);// 下方向の速度を設定
+
+       
+
+        }else if(cursors.left.isDown){
+
+            console.log("Left");
+
+            this.TARO.setVelocityX(-40);// 左方向の速度を設定
+            this.HANAKO.setVelocityX(40);// 左方向の速度を設定
+
+
+        }else if(cursors.right.isDown){
+
+            console.log("Right!!");
+
+            this.TARO.setVelocityX(40);// 右方向の速度を設定
+            this.HANAKO.setVelocityX(-40);// 右方向の速度を設定
+
+
+            
+        }else{
+
+            this.TARO.setVelocityX(0);// 横方向の速度を0
+            this.HANAKO.setVelocityX(0);// 横方向の速度を0
+
+            this.TARO.setVelocityY(0);// 縦方向の速度を0
+            this.HANAKO.setVelocityY(0);// 縦方向の速度を0
+
         }
+
+
     }
-}
+    }
